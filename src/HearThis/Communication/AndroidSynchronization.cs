d415ceb33993e3824b6452177565218046e4febe
Wade Mergenthal <wmergenthal@gmail.com>
2025-04-24 20:54:06 -0400

    First cut at porting Bloom fix for incorrect IP addr selection
    It's a bit rough still but it gives the right answer for a wired connection, including that the QR code contains the correct IP address. Next is to try it on WiFi, then clean up the code.

--- src/HearThis/Communication/AndroidSynchronization.cs
+++ src/HearThis/Communication/AndroidSynchronization.cs
@@ -21,6 +21,9 @@ using DesktopAnalytics;
 using L10NSharp;
 using SIL.Reporting;
 using static System.String;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using NDesk.DBus;
 
 namespace HearThis.Communication
 {
@@ -30,6 +33,56 @@ namespace HearThis.Communication
 	public static class AndroidSynchronization
 	{
 		private const string kHearThisAndroidProductName = "HearThis Android";
+
+		// Layout of a row in the IPv4 routing table.
+		[StructLayout(LayoutKind.Sequential)]
+		public struct MIB_IPFORWARDROW
+		{
+			public uint dwForwardDest;
+			public uint dwForwardMask;
+			public uint dwForwardPolicy;
+			public uint dwForwardNextHop;
+			public int dwForwardIfIndex;
+			public int dwForwardType;
+			public int dwForwardProto;
+			public int dwForwardAge;
+			public int dwForwardNextHopAS;
+			public int dwForwardMetric1;  // the "interface metric" we need
+			public int dwForwardMetric2;
+			public int dwForwardMetric3;
+			public int dwForwardMetric4;
+			public int dwForwardMetric5;
+		}
+
+		// Holds a copy of the IPv4 routing table, which we will examine
+		// to find which row/route has the lowest "interface metric".
+		[StructLayout(LayoutKind.Sequential)]
+		private struct MIB_IPFORWARDTABLE
+		{
+			private int dwNumEntries;
+			private MIB_IPFORWARDROW table;
+		}
+
+		// We use an unmanaged function in the C/C++ DLL "iphlpapi.dll".
+		//   - "true": calling this function *can* set an error code,
+		//     which will be retrieveable via Marshal.GetLastWin32Error()
+		[DllImport("iphlpapi.dll", SetLastError = true)]
+		static extern int GetIpForwardTable(IntPtr pIpForwardTable, ref int pdwSize, bool bOrder);
+
+		// Holds relevant network interface attributes.
+		private class InterfaceInfo
+		{
+			public string IpAddr  { get; set; }
+			//public string NetMask { get; set; }  // Bloom needs this, HearThis does not
+			public string Type    { get; set; }
+			public int Metric     { get; set; }
+		}
+
+		// Holds the current network interface candidate. After all interfaces are
+		// checked this will hold the one having the lowest interface metric (the
+		// "winner") which is the one Windows will choose for network traffic.
+		static InterfaceInfo IfaceWinner = new InterfaceInfo();
+
 		public static void DoAndroidSync(Project project, Form parent)
 		{
 			if (!project.IsRealProject)
@@ -42,9 +95,25 @@ namespace HearThis.Communication
 				return;
 			}
 			var dlg = new AndroidSyncDialog();
-			var address = GetNetworkAddress(parent);
 
-			if (address == null)
+			// WM, DEBUG ONLY
+			// Current local IP determination logic does consider network interfaces but it
+			// does not always settle on the correct one. My Windows 10 laptop is an example.
+			//
+			// Instructive exercise: uncomment and run this debug function, ShowNetworkInterfaces(),
+			// to list all network interfaces along with significant attributes of each including IP
+			// addresses (an interface can have several).
+			ShowNetworkInterfaces();
+			// WM, END DEBUG
+
+			Debug.WriteLine("AndroidSynchronization, calling GetInterfaceStackWillUse()");
+			GetInterfaceStackWillUse(parent);
+			//Debug.WriteLine("AndroidSynchronization, local IP = " + IfaceWinner.IpAddr + " (" + IfaceWinner.Type + ")");
+			Debug.WriteLine("AndroidSynchronization, local IP = {0} ({1})", IfaceWinner.IpAddr, IfaceWinner.Type);
+			//var address = GetNetworkAddress(parent);
+			var address = IfaceWinner.IpAddr;
+
+			if (address == null)   // do we really need this?
 				return;
 
 			dlg.SetOurIpAddress(address.ToString());
@@ -138,9 +207,17 @@ namespace HearThis.Communication
 			dlg.Show(parent);
 		}
 
-		private static IPAddress GetNetworkAddress(Form parent)
+		private static void GetInterfaceStackWillUse(Form parent)
 		{
-			// Retrieve all network interfaces
+			int currentIfaceMetric;
+			//int maxInt = int.MaxValue;
+
+			// Initialize result struct's metric field to the highest possible value
+			// so the first interface metric value seen will always replace it.
+			IfaceWinner.Metric = int.MaxValue;
+			//IfaceWinner.Metric = maxInt;
+
+			// Retrieve all network interfaces that are *active*
 			var allOperationalNetworks = NetworkInterface.GetAllNetworkInterfaces()
 				.Where(ni => ni.OperationalStatus == OperationalStatus.Up).ToArray();
 
@@ -149,42 +226,173 @@ namespace HearThis.Communication
 				MessageBox.Show(parent, LocalizationManager.GetString("AndroidSynchronization.NetworkingRequired",
 					"Android synchronization requires your computer to have networking enabled."),
 					Program.kProduct);
-				return null;
+				Debug.WriteLine("AndroidSynchronization, no network interfaces are operational");
+				//return null;
+				return;
 			}
 
-			(NetworkInterface Network, IPAddress Address)? preferred = null;
-
-			foreach (var network in allOperationalNetworks)
+			// Get key attributes of active network interfaces.
+			foreach (NetworkInterface ni in allOperationalNetworks)
 			{
-				var ipAddress = network.GetIPProperties().UnicastAddresses.Select(ip => ip.Address)
-					.FirstOrDefault(a => a.AddressFamily == AddressFamily.InterNetwork);
-
-				if (ipAddress != null)
+				// If we can't get IP or IPv4 properties for this interface, skip it.
+				var ipProps = ni.GetIPProperties();
+				if (ipProps == null)
 				{
-					if (network.NetworkInterfaceType == NetworkInterfaceType.Wireless80211)
-					{
-						preferred = (network, ipAddress);
-						break;
-					}
+					continue;
+				}
+				var ipv4Props = ipProps.GetIPv4Properties();
+				if (ipv4Props == null)
+				{
+					continue;
+				}
 
-					if (preferred == null)
-						preferred = (network, ipAddress);
+				Debug.WriteLine("AndroidSynchronization, checking IP addresses in " + ni.Name);
+				foreach (UnicastIPAddressInformation ip in ipProps.UnicastAddresses)
+				{
+					// We don't consider IPv6 so filter for IPv4 ('InterNetwork')
+					if (ip.Address.AddressFamily == AddressFamily.InterNetwork)
+					{
+						currentIfaceMetric = GetMetricForInterface(ipv4Props.Index);
+
+						// If this interface's metric is lower than what we've seen
+						// so far, save it and its relevant associated values.
+						if (currentIfaceMetric < IfaceWinner.Metric)
+						{
+							IfaceWinner.IpAddr = ip.Address.ToString();
+							//IfaceWinner.NetMask = ip.IPv4Mask.ToString();
+							IfaceWinner.Type = ni.NetworkInterfaceType.ToString();
+							IfaceWinner.Metric = currentIfaceMetric;
+						}
+					}
 				}
 			}
+		}
 
-			if (!preferred.HasValue)
+		// Get a key piece of info ("metric") from the specified network interface.
+		// https://learn.microsoft.com/en-us/windows/win32/api/iphlpapi/nf-iphlpapi-getipforwardtable
+		//
+		// Retrieving the metric is not as simple as grabbing one of the fields in
+		// the network interface. The metric resides in the network stack routing
+		// table. One of the interface fields ("Index") is also in the routing table
+		// and is how we correlate the two.
+		//   - Calling code (walking the interface collection) passes in the index
+		//     of the interface whose "best" metric it wants.
+		//   - This function walks the routing table looking for all rows (each of
+		//     which is a route) containing that index. It notes the metric in each
+		//     route and returns the lowest among all routes/rows for the interface.
+		//
+		private static int GetMetricForInterface(int interfaceIndex)
+		{
+			int bestMetric;
+
+			// Preliminary: call with a null buffer ('size') to learn how large a
+			// buffer is needed to hold a copy of the routing table.
+			int size = 0;
+			GetIpForwardTable(IntPtr.Zero, ref size, false);
+
+			// 'size' now shows how large a buffer is needed, so allocate it.
+			IntPtr tableBuf = Marshal.AllocHGlobal(size);
+
+			try
 			{
-				MessageBox.Show(parent, LocalizationManager.GetString("AndroidSynchronization.NoInterNetworkIPAddress",
-					"Sorry, your network adapter has no InterNetwork IP address. If you do not know how to resolve this, please seek technical help.",
-					Program.kProduct));
-				return null;
+				// Copy the routing table into buffer for examination.
+				// If the result code is not 0 then something went wrong.
+				int error = GetIpForwardTable(tableBuf, ref size, false);
+				if (error != 0)
+				{
+					// It is tempting to add a dealloc call here before bailing, but
+					// don't. The dealloc in the finally-block *will* be done (I checked).
+					Console.WriteLine("  GetMetricForInterface, ERROR, GetIpForwardTable() = {0}, returning {1}", error, int.MaxValue);
+					return int.MaxValue;
+				}
+
+				// Get number of routing table entries.
+				int numEntries = Marshal.ReadInt32(tableBuf);
+
+				// Advance pointer past the integer to point at 1st row.
+				IntPtr rowPtr = IntPtr.Add(tableBuf, 4);
+
+				// Initialize to "worst" possible metric (Win10 Pro: 2^31 - 1).
+				// It can only get better from there!
+				bestMetric = int.MaxValue;
+
+				// Walk the routing table looking for rows involving the the network
+				// interface passed in. For each such row/route, check the metric.
+				// If it is lower than the lowest we've yet seen, save it to become
+				// the new benchmark.
+				for (int i = 0; i < numEntries; i++)
+				{
+					MIB_IPFORWARDROW row = Marshal.PtrToStructure<MIB_IPFORWARDROW>(rowPtr);
+					if (row.dwForwardIfIndex == interfaceIndex)
+					{
+						bestMetric = Math.Min(bestMetric, row.dwForwardMetric1);
+					}
+					rowPtr = IntPtr.Add(rowPtr, Marshal.SizeOf<MIB_IPFORWARDROW>());
+				}
+			}
+			finally
+			{
+				Marshal.FreeHGlobal(tableBuf);
 			}
 
-			Logger.WriteEvent("Found " +
-				$"{(preferred.Value.Network.NetworkInterfaceType == NetworkInterfaceType.Wireless80211 ? "a" : "only a wired")}" +
-				$" network for Android synchronization: {preferred.Value.Network.Description}.");
-			
-			return preferred.Value.Address;
+			return bestMetric;
 		}
+
+		// WM, DEBUG ONLY
+		// Ported from temporary Bloom dev code: broadcast_address_buildup_test_02.cs
+		// Unlike for BloomDesktop, HearThis doesn't need to identify which network interface is being
+		// used because HearThis won't be transmitting on it to share its local IP. The QR code does
+		// that instead. Still, it is interesting (and likely helpful) to see the available network
+		// interfaces and their main attributes.
+		//
+		private static void ShowNetworkInterfaces()
+		{
+			Debug.WriteLine("AndroidSynchronization, ShowNetworkInterfaces:");
+
+			// index i: initialize to 1 less than usual so its increment can occur at the
+			//          top of a loop, and thus not get missed after a 'continue'
+			int i = -1;
+
+			foreach (NetworkInterface nic in NetworkInterface.GetAllNetworkInterfaces())
+			{
+				Debug.WriteLine("  ----------");
+				i++;
+				Debug.WriteLine("  nic[" + i + "].Name = " + nic.Name);
+				Debug.WriteLine("  nic[" + i + "].Id   = " + nic.Id);
+				Debug.WriteLine("  nic[" + i + "].Description = " + nic.Description);
+				Debug.WriteLine("  nic[" + i + "].NetworkInterfaceType = " + nic.NetworkInterfaceType);
+				Debug.WriteLine("  nic[" + i + "].OperationalStatus = " + nic.OperationalStatus);
+				Debug.WriteLine("  nic[" + i + "].Speed = " + nic.Speed);
+
+				if (!nic.Supports(NetworkInterfaceComponent.IPv6))
+				{
+					Debug.WriteLine("    does not support IPv6"); // on my machine only Wi-Fi has this
+				}
+				if (!nic.Supports(NetworkInterfaceComponent.IPv4))
+				{
+					Debug.WriteLine("    does not support IPv4"); // on my machine none show this
+					continue;
+				}
+
+				IPInterfaceProperties ipProps = nic.GetIPProperties();
+				IPv4InterfaceProperties ipPropsV4 = ipProps.GetIPv4Properties();
+				if (ipPropsV4 == null)
+				{
+					Debug.WriteLine("    IPv4 information not available"); // on my machine none show this
+					continue;
+				}
+
+				// Show all IP addresses held by this network interface. Also show all
+				// IPv4 netmasks. Note that IPv4 netmasks contain all zeroes for IPv6
+				// addresses (which we don't use).
+				foreach (UnicastIPAddressInformation addr in ipProps.UnicastAddresses)
+				{
+					Debug.WriteLine("  nic[" + i + "], ipProps.addr.Address  = " + addr.Address.ToString());
+					Debug.WriteLine("  nic[" + i + "], ipProps.addr.IPv4Mask = " + addr.IPv4Mask);
+				}
+			}
+			Debug.WriteLine("AndroidSynchronization, ShowNetworkInterfaces: DONE");
+		}
+		// WM, END DEBUG
 	}
 }
